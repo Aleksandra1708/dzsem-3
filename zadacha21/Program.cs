c63bda3c2f700b4b0a3581f6d8c8946d3d4d2346
Alaksandra Pavlova <wellessa1717@gmail.com>
2023-04-15 01:30:13 +0300

    задача21

---
+++ zadacha21/Program.cs
@@ -0,0 +1,19 @@
+﻿int x1 = ReadInt("Введите х для первой точки: ");
+int y1 = ReadInt("Введите y для первой точки: ");
+int z1 = ReadInt("Введите z для первой точки: ");
+int x2 = ReadInt("Введите х для второй точки: ");
+int y2 = ReadInt("Введите y для второй точки: ");
+int z2 = ReadInt("Введите z для второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z2 - z1;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine("Расстояние = " + length);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
